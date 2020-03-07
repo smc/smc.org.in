69fdb1f0e9daacacef6bfcc7c251858f560837b0
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-03-07 17:29:12 +0530

    Set application title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title =
+        "Swathanthra Malayalam Computing | സ്വതന്ത്ര മലയാളം കമ്പ്യൂട്ടിങ്ങ്";
+      return args;
+    });
+  }
 };
