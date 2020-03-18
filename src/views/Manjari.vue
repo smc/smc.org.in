935b3dd768299cd805939c79f8e85ee44294ecc5
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-03-18 22:24:29 +0530

    Manjari page updates

--- src/views/Manjari.vue
+++ src/views/Manjari.vue
@@ -141,6 +141,19 @@
           v-text="$i18n('Supported languages')"
           class="col-lg-4 col-md-12"
         ></h2>
+        <div class="col-lg-8 col-md-12">
+          <ul>
+            <li>Malayalam</li>
+            <li>
+              Latin. The following languages are covered: aa, ay, bi, br, ch,
+              da, de, en, es, et, eu, fi, fj, fo, fur, fy, gd, gl, gv, ho, ia,
+              id, ie, io, is, it, ki, lb, mg, nb, nds, nl, nn, no, nr, nso, ny,
+              oc, om, pt, rm, sma, smj, so, sq, ss, st, sv, sw, tk, tl, tn, ts,
+              uz, vo, vot, wa, xh, yap, zu, an, fil, ht, jv, kj, ku-tr, kwm, li,
+              ms, na, ng, pap-an, pap-aw, rn, rw, sc, sg, sn, su, and za(s)
+            </li>
+          </ul>
+        </div>
       </section>
       <section id="style-variants" class="row">
         <h2 v-text="$i18n('Style variants')" class="col-lg-4 col-md-12"></h2>
@@ -151,7 +164,18 @@
       </section>
       <section id="faq" class="row">
         <h2 v-text="$i18n('faq')" class="col-lg-4 col-md-12"></h2>
-        <p class="col-lg-8 col-md-12">Questions and answers</p>
+        <div class="col-lg-8 col-md-12">
+          <h4 id="contribute">Can I help with improving Manjari?</h4>
+          <p>
+            Yes you can! Manjari is an open-source project, meaning the source
+            code—or "source design" if you will—that is used to build the font
+            files are freely available to improve upon. Font making requires a
+            fair bit of technical work and depending on what you'd like to do,
+            some things might be more fun depending on your technical skills.
+            Please start by reading and exploring the source code and design
+            files provided.
+          </p>
+        </div>
       </section>
     </main>
   </v-container>
