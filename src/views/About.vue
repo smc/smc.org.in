ff2a786a013d3da4ba6022139d08b936982bc939
Ashik Salahudeen <ashik@inflo.ws>
2020-04-11 01:00:39 +0800

    Added content to About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,19 @@
 <template>
   <v-container>
-    <div class="about">
-      <h1>This is an about page</h1>
-    </div>
+    <v-row class="text-center">
+      <h1 class="title" v-text="$i18n('smc-about-heading')"></h1>
+    </v-row>
+    <v-row >
+      <v-col> <p v-text="$i18n('smc-desc')"></p> </v-col>
+    </v-row>
+    <v-row>
+        <v-col> <p v-text="$i18n('smc-about-text-para-1')"></p> </v-col>
+      </v-row>
+    <v-row>
+      <v-col> <p v-text="$i18n('smc-about-text-para-2')"></p> </v-col>
+    </v-row>
+    <v-row>
+      <v-col> <p v-text="$i18n('smc-about-text-para-3')"></p> </v-col>
+    </v-row>
   </v-container>
 </template>
