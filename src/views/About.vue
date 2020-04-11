efe7a65774d7c3782714144ec1e70b3dd756eeb1
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-04-11 09:42:33 +0530

    Format and simplify About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,11 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <h1 class="title" v-text="$i18n('smc-about-heading')"></h1>
-    </v-row>
-    <v-row >
-      <v-col> <p v-text="$i18n('smc-desc')"></p> </v-col>
-    </v-row>
-    <v-row>
-        <v-col> <p v-text="$i18n('smc-about-text-para-1')"></p> </v-col>
-      </v-row>
-    <v-row>
-      <v-col> <p v-text="$i18n('smc-about-text-para-2')"></p> </v-col>
-    </v-row>
-    <v-row>
-      <v-col> <p v-text="$i18n('smc-about-text-para-3')"></p> </v-col>
-    </v-row>
+  <v-container class="col-lg-8 col-sm-12">
+    <h1 class="display-3 my-4" v-text="$i18n('smc-about-heading')"></h1>
+    <article>
+      <p v-text="$i18n('smc-desc')"></p>
+      <p v-text="$i18n('smc-about-text-para-1')"></p>
+      <p v-text="$i18n('smc-about-text-para-2')"></p>
+      <p v-text="$i18n('smc-about-text-para-3')"></p>
+    </article>
   </v-container>
 </template>
