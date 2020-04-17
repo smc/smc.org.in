d413376183bd625282f7efb66c0500c91c3d6a0e
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-04-17 17:24:28 +0530

    Page fixups

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,6 @@
 <template>
-  <v-container>
-    <div class="404">
-      <h1>Page not found</h1>
-    </div>
+  <v-container class="contact col-lg-6 col-md-8 col-sm-12">
+    <h1>Page not found</h1>
+    <v-img height="80vh" cover src="https://picsum.photos/400" />
   </v-container>
 </template>
