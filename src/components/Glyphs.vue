e9482731ba72eb90a4eb40b59d01bf18b3e38a7f
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-04-17 15:52:24 +0530

    Glyphs: Bottom navigation layout

--- src/components/Glyphs.vue
+++ src/components/Glyphs.vue
@@ -1,11 +1,6 @@
 <template>
   <section class="glyphs">
-    <v-bottom-sheet
-      inset
-      hide-overlay
-      :key="glyph.code"
-      v-for="glyph in glyphs"
-    >
+    <v-bottom-sheet inset :key="glyph.code" v-for="glyph in glyphs">
       <template v-slot:activator="{ on }">
         <v-btn
           class="glyph title"
@@ -18,43 +13,59 @@
         >
         </v-btn>
       </template>
-      <v-sheet class="text-center pa-4">
-        <div
-          class="display-4 py-4 text-center"
-          :style="`font-family:${font} !important;`"
-          v-text="`${glyph.value}`"
-        ></div>
-        <v-btn v-if="canCopy" @click="copy(glyph.value)">Copy</v-btn>
-        <div class="title" v-text="`Glyph name: ${glyph.name}`"></div>
-        <div
-          class="title"
-          v-if="glyph.code"
-          v-text="`Code: ${glyph.code}`"
-        ></div>
-        <div
-          class="title"
-          v-if="glyph.sequence"
-          v-text="`Opentype feature: ${glyph.sequence.features}`"
-        ></div>
-        <div
-          class="title"
-          v-if="glyph.sequence"
-          v-text="`Glyphs: ${glyph.sequence.src}`"
-        ></div>
-        <div
-          class="title"
-          v-if="glyph.mlsequence"
-          v-text="`${glyph.mlsequence}`"
-        ></div>
+      <v-sheet class=" pa-4">
+        <v-row>
+          <v-col class="text-center">
+            <div
+              class="display-4 py-4 text-center"
+              :style="`font-family:${font} !important;`"
+              v-text="`${glyph.value}`"
+            ></div>
+          </v-col>
+          <v-col>
+            <div class="subtitle-2" v-text="`Glyph name: ${glyph.name}`"></div>
+            <div
+              class="subtitle-2"
+              v-if="glyph.code"
+              v-text="`Code: ${glyph.code}`"
+            ></div>
+            <div
+              class="subtitle-2"
+              v-if="glyph.sequence"
+              v-text="`Opentype feature: ${glyph.sequence.features}`"
+            ></div>
+            <div
+              class="subtitle-2"
+              v-if="glyph.sequence"
+              v-text="`Glyphs: ${glyph.sequence.src}`"
+            ></div>
+            <div
+              class="subtitle-2"
+              v-if="glyph.mlsequence"
+              v-text="`${glyph.mlsequence}`"
+            ></div>
+            <v-btn
+              icon
+              round
+              title="Copy to clipboard"
+              v-if="canCopy"
+              @click="copy(glyph.value)"
+            >
+              <v-icon>{{ mdiContentCopy }}</v-icon></v-btn
+            >
+          </v-col>
+        </v-row>
       </v-sheet>
     </v-bottom-sheet>
   </section>
 </template>
 <script>
+import { mdiContentCopy } from "@mdi/js";
 export default {
   name: "Glyphs",
   data: () => ({
-    canCopy: false
+    canCopy: false,
+    mdiContentCopy
   }),
   props: {
     font: String,
