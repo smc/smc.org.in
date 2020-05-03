d1a7ec69c66315f2a6dbaf630c935284c3d32766
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-05-03 20:44:19 +0530

    Fix projects layout

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,8 +4,7 @@
       <h1 class="display-3 my-10" v-i18n="'menu-projects'"></h1>
 
       <section class="row">
-        <v-col lg="3" md="6" xs="12" class="my-2">
-          <v-card outlined height="400px" class="d-flex flex-column" hover>
+          <v-card outlined height="400px" class="col-lg-5 col-md-12 ma-4" hover>
             <v-img
               height="200px"
               contain
@@ -26,10 +25,8 @@
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
 
-        <v-col lg="3" md="6" xs="12" class="my-2">
-          <v-card outlined height="400px" class="d-flex flex-column" hover>
+          <v-card outlined height="400px" class="col-lg-5 col-md-12 ma-4" hover>
             <v-card-title>Varnam</v-card-title>
 
             <v-card-text class="text--primary flex-grow-1">
@@ -44,10 +41,8 @@
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
 
-        <v-col lg="3" md="6" xs="12" class="my-2">
-          <v-card outlined height="400px" class="d-flex flex-column" hover>
+          <v-card outlined height="400px" class="col-lg-5 col-md-12 ma-4" hover>
             <v-card-title>Libindic</v-card-title>
 
             <v-card-text class="text--primary flex-grow-1">
@@ -66,8 +61,7 @@
           </v-card>
         </v-col>
 
-        <v-col lg="3" md="6" xs="12" class="my-2">
-          <v-card outlined height="400px" class="d-flex flex-column" hover>
+          <v-card outlined height="400px" class="col-lg-5 col-md-12 ma-4" hover>
             <v-card-title>Dhvani</v-card-title>
 
             <v-card-text class="text--primary flex-grow-1">
@@ -85,10 +79,7 @@
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
-
-        <v-col lg="3" md="6" xs="12" class="my-2">
-          <v-card outlined height="400px" class="d-flex flex-column" hover>
+          <v-card outlined height="400px" class="col-lg-5 col-md-12 ma-4"  hover>
             <v-card-title>ibus-sharada-braille</v-card-title>
 
             <v-card-text class="text--primary flex-grow-1">
@@ -107,10 +98,8 @@
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
 
-        <v-col lg="3" md="6" xs="12" class="my-2">
-          <v-card outlined height="400px" class="d-flex flex-column" hover>
+          <v-card outlined height="400px" class="col-lg-5 col-md-12 ma-4" hover>
             <v-card-title>Grandham</v-card-title>
 
             <v-card-text class="text--primary flex-grow-1">
@@ -127,7 +116,6 @@
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-col>
       </section>
     </div>
   </v-container>
