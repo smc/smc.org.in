5c5e254f28b6d9e6a7f02f454a45012cb6d0ee1c
Jishnu Mohan <jishnu7@gmail.com>
2020-05-04 11:47:47 +0530

    create sections under projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,45 +1,51 @@
 <template>
   <v-container class="col-xl-8 col-sm-10 col-xs-12">
-    <div class="projects">
+    <article class="projects">
       <h1 class="display-3 my-10" v-i18n="'menu-projects'"></h1>
 
-      <section class="row">
-        <v-col
-          cols="12"
-          xl="3"
-          lg="4"
-          md="6"
-          class="my-2"
-          v-for="project in projects"
-          :key="project.title"
-        >
-          <v-card outlined class="d-flex flex-column">
-            <v-img
-              v-if="project.logo"
-              height="200px"
-              contain
-              :src="project.logo"
-            />
-            <v-card-title>{{ project.title }}</v-card-title>
-            <v-card-text class="text--primary flex-grow-1">
-              {{ project.description }}
-            </v-card-text>
+      <section
+        v-for="section in projects"
+        :key="section.heading">
+        <h2>{{ section.heading }}</h2>
 
-            <v-card-actions>
-              <v-btn
-                color="primary"
-                text
-                v-for="link in project.links"
-                :key="link.text"
-                :href="link.url"
-              >
-                {{ link.text }}
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
+        <section class="row">
+          <v-col
+            cols="12"
+            xl="3"
+            lg="4"
+            md="6"
+            class="my-2"
+            v-for="project in section.list"
+            :key="project.title"
+          >
+            <v-card outlined class="d-flex flex-column">
+              <v-img
+                v-if="project.logo"
+                height="200px"
+                contain
+                :src="project.logo"
+              />
+              <v-card-title>{{ project.title }}</v-card-title>
+              <v-card-text class="text--primary flex-grow-1">
+                {{ project.description }}
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  color="primary"
+                  text
+                  v-for="link in project.links"
+                  :key="link.text"
+                  :href="link.url"
+                >
+                  {{ link.text }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </section>
       </section>
-    </div>
+    </article>
   </v-container>
 </template>
 
@@ -63,93 +69,120 @@ export default {
   data: () => ({
     projects: [
       {
-        logo: require("@/assets/images/indickeyboard.png"),
-        title: "Indic Keyboard",
-        description: `
-          Award-winning Indian Language keyboard application for Android
-          phones that supports 23 languages.
-        `,
-        links: [
+        heading: "Input Tools",
+        list: [
           {
-            text: "Details",
-            url: "https://indic.app"
+            logo: require("@/assets/images/indickeyboard.png"),
+            title: "Indic Keyboard",
+            description: `
+              Award-winning Indian Language keyboard application for Android
+              phones that supports 23 languages.
+            `,
+            links: [
+              {
+                text: "Details",
+                url: "https://indic.app"
+              }
+            ]
+          },
+          {
+            title: "Varnam",
+            description: `
+              Varnam is a cross platform predictive transliterator for Indic
+              languages. Varnam aims at providing consistent input experience
+              across different platforms in almost all indic languages.
+            `,
+            links: [
+              {
+                text: "Details",
+                url: "https://www.varnamproject.com/"
+              }
+            ]
+          },
+          {
+            title: "Libindic",
+            description: `
+              Libindic is a web platform to host Free Software language
+              processing applications. It consists of a web framework and a set
+              of applications for processing Indian languages. It is a platform
+              for porting existing and upcoming language processing applications
+              to the web.
+            `,
+            links: [
+              {
+                text: "Details",
+                url: "https://libindic.org/"
+              }
+            ]
+          },
+          {
+            title: "Dhvani",
+            description: `
+              Dhvani is a text to speech system designed for Indian Languages.
+              It is helpful for the visually challenged users as a screenreader
+              in their mother tongue. Currently dhvani is capable of generating
+              intelligible speech for Bengali, Gujarati, Hindi, Kannada,
+              Malayalam, Marathi, Oriya, Panjabi, Tamil, Telugu,
+              Pashto(experimental)
+            `,
+            links: [
+              {
+                text: "Details",
+                url: "http://dhvani.sourceforge.net/"
+              }
+            ]
+          },
+          {
+            title: "ibus-sharada-braille",
+            description: `
+              ibus-sharada-braille(ISB) is an ibus input engine based on six key
+              approach of braille. It currently supports seven languages
+              English, Malayalam, Hindi, Kannada, Tamil, French, and Spanish.
+            `,
+            links: [
+              {
+                text: "Details",
+                url: "https://github.com/Nalin-x-Linux/ibus-sharada-braille"
+              }
+            ]
+          },
+          {
+            title: "Grandham",
+            description: `
+              Grandham.org is an effort to build crowdsourced bibliography
+              datapool. It suppports all indian languages and have interfaces
+              and apis fo library, publishers and general public. The
+              Malayalagrandham dataset the biggest bibliography data set in
+              malayalam and may be the largest malayalam Open data archive.
+            `,
+            links: [
+              {
+                text: "Details",
+                url: "https://grandham.in/"
+              }
+            ]
           }
         ]
       },
       {
-        title: "Varnam",
-        description: `
-          Varnam is a cross platform predictive transliterator for Indic
-          languages. Varnam aims at providing consistent input experience
-          across different platforms in almost all indic languages.
-        `,
-        links: [
+        heading: "Web Extensions",
+        list: [
           {
-            text: "Details",
-            url: "https://www.varnamproject.com/"
-          }
-        ]
-      },
-      {
-        title: "Libindic",
-        description: `
-          Libindic is a web platform to host Free Software language
-          processing applications. It consists of a web framework and a set
-          of applications for processing Indian languages. It is a platform
-          for porting existing and upcoming language processing applications
-          to the web.
-        `,
-        links: [
-          {
-            text: "Details",
-            url: "https://libindic.org/"
-          }
-        ]
-      },
-      {
-        title: "Dhvani",
-        description: `
-          Dhvani is a text to speech system designed for Indian Languages.
-          It is helpful for the visually challenged users as a screenreader
-          in their mother tongue. Currently dhvani is capable of generating
-          intelligible speech for Bengali, Gujarati, Hindi, Kannada,
-          Malayalam, Marathi, Oriya, Panjabi, Tamil, Telugu,
-          Pashto(experimental)
-        `,
-        links: [
-          {
-            text: "Details",
-            url: "http://dhvani.sourceforge.net/"
-          }
-        ]
-      },
-      {
-        title: "ibus-sharada-braille",
-        description: `
-          ibus-sharada-braille(ISB) is an ibus input engine based on six key
-          approach of braille. It currently supports seven languages
-          English, Malayalam, Hindi, Kannada, Tamil, French, and Spanish.
-        `,
-        links: [
-          {
-            text: "Details",
-            url: "https://github.com/Nalin-x-Linux/ibus-sharada-braille"
-          }
-        ]
-      },
-      {
-        title: "Grandham",
-        description: `
-          Grandham.org is an effort to build crowdsourced bibliography
-          datapool. It suppports all indian languages and have interfaces
-          and apis fo library, publishers and general public. The
-          Malayalagrandham dataset the biggest bibliography data set in
-          malayalam and may be the largest malayalam Open data archive.
-        `,
-        links: [
-          {
-            text: "Details",
-            url: "https://grandham.in/"
+            title: "Malayalam Fonts",
+            description: `
+              This addon will help you to choose Unicode Malayalam fonts for the websites you visit,
+              without installing anything on your phone or computer.
+            `,
+            links: [
+              {
+                text: "Firefox",
+                url: "https://addons.mozilla.org/en-US/firefox/addon/malayalam-fonts/"
+              },
+              {
+                text: "Chrome",
+                url: "https://chrome.google.com/webstore/detail/malayalam-font/jgdfkpocgoliiikomkacimfdkedjdgfo"
+              }
+            ]
           }
         ]
       }
