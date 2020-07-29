72c215c49f40ab4da301d5c26ae7e4d4856488a2
Joice M. Joseph <joice@moolekkari.net>
2020-07-29 10:41:49 +0530

    starting to add ui for press and media link

---
+++ src/views/Media.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row class="mb-6" justify="center">
+    <v-col xl="8" sm="10" xs="10">
+      <section class="pa-2">
+        <h1 class="text-h2 my-10" v-i18n="'media'"></h1>
+
+        <!--  -->
+      </section>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Media",
+  components: {}
+};
+</script>
