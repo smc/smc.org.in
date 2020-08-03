7924a9a2525a88fe75c4546fdbee6d8e461971b3
Joice M. Joseph <joice@moolekkari.net>
2020-08-03 21:02:11 +0530

    added python-venv in package list

--- tools/generate_page_content.sh
+++ tools/generate_page_content.sh
@@ -2,7 +2,7 @@
 
 MEDIA_SOURCE_FILE=${MEDIA_SOURCE_FILE:-"./press_archive.csv"}
 MEDIA_OUTPUT_FILE=${MEDIA_OUTPUT_FILE:-"./../public/press/media.json"}
-echo $(pwd)
+
 mkdir -p public/press
 
 cd tools
