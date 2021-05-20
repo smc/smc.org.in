98d2b5d3b3beebd8bf316800c6098c6824071254
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-05-20 17:07:52 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint",
+    ecmaVersion: 2017,
+    sourceType: "module",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
