43ec6f614a4aeaaae2d45f840ec2b8643238a7fc
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2022-06-07 16:24:37 +0530

    Revert "Experiment: Define pages per routes for SEO"
    This reverts commit 6831146a3323e85e7b5221e4aba6218ba1dd44d2.

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,10 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  pages: {
-    index: {
-      // entry for the page
-      entry: "src/main.js",
-      // the source template
-      template: "public/index.html",
-      // output as dist/index.html
-      filename: "index.html",
-    },
-    fonts: {
-      entry: "src/main.js",
-      filename: "fonts/index.html",
-    },
-    manjari: {
-      entry: "src/main.js",
-      filename: "fonts/manjari/index.html",
-    },
-    projects: {
-      entry: "src/main.js",
-      filename: "projects/index.html",
-    },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title =
+        "Swathanthra Malayalam Computing | സ്വതന്ത്ര മലയാളം കമ്പ്യൂട്ടിങ്ങ്";
+      return args;
+    });
   },
 };
