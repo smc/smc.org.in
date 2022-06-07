8611cb491a418d4c058727f538da77080ad9cb8e
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2022-06-07 16:24:24 +0530

    Revert "SEO: Add more entry pages"
    This reverts commit 2fd700fe9db73c3813b79519534efb29f64b9f79.

--- vue.config.js
+++ vue.config.js
@@ -9,26 +9,6 @@ module.exports = {
       // output as dist/index.html
       filename: "index.html",
     },
-    blog: {
-      entry: "src/main.js",
-      filename: "blog/index.html",
-    },
-    articles: {
-      entry: "src/main.js",
-      filename: "articles/index.html",
-    },
-    research: {
-      entry: "src/main.js",
-      filename: "research/index.html",
-    },
-    about: {
-      entry: "src/main.js",
-      filename: "about/index.html",
-    },
-    contact: {
-      entry: "src/main.js",
-      filename: "contact/index.html",
-    },
     fonts: {
       entry: "src/main.js",
       filename: "fonts/index.html",
@@ -36,18 +16,6 @@ module.exports = {
     manjari: {
       entry: "src/main.js",
       filename: "fonts/manjari/index.html",
-    },
-    gayathri: {
-      entry: "src/main.js",
-      filename: "fonts/gayathri/index.html",
-    },
-    chilanka: {
-      entry: "src/main.js",
-      filename: "fonts/chilanka/index.html",
-    },
-    dyuthi: {
-      entry: "src/main.js",
-      filename: "fonts/dyuthi/index.html",
     },
     projects: {
       entry: "src/main.js",
